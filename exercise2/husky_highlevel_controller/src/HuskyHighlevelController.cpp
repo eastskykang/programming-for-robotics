7a711af258ca7dfcf6bdff8044b5500f44cda2b8
kangdongho <east0822@gmail.com>
2017-02-23 12:08:03 +0100

    config and getparam added

--- exercise2/husky_highlevel_controller/src/HuskyHighlevelController.cpp
+++ exercise2/husky_highlevel_controller/src/HuskyHighlevelController.cpp
@@ -7,7 +7,16 @@ HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) 
 {
   subscriber_ = nodeHandle.subscribe("/scan", 10, &HuskyHighlevelController::topicCallback, this);
 
+  std::string topic;
+  int queue_size;
 
+  if(!nodeHandle.getParam("topic", topic)) {
+    ROS_ERROR("Could not find topic parameter!");
+  }
+
+  if (!nodeHandle.getParam("queue_size", queue_size)) {
+    ROS_ERROR("Could not find queue_size parameter!");
+  }
 }
 
 HuskyHighlevelController::~HuskyHighlevelController()
@@ -19,7 +28,7 @@ void HuskyHighlevelController::topicCallback(const sensor_msgs::LaserScan::Const
   int i = 0;
   int size = msg->ranges.size();
 
-  float min_dis = 1000;
+  float min_dis = 10000;
 
   for (i = 0; i < size; i++)
   {
