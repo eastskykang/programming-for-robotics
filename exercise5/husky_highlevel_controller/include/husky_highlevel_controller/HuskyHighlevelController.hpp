787b10e4a0718dd520600f2a710ca96fcb2edd32
donghokang <east0822@gmail.com>
2017-02-28 16:45:24 +0100

    exercise5 added

---
+++ exercise5/husky_highlevel_controller/include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
+#include <visualization_msgs/Marker.h>
+
+namespace husky_highlevel_controller {
+
+/*!
+ * Class containing the Husky Highlevel Controller
+ */
+class HuskyHighlevelController {
+public:
+	/*!
+	 * Constructor.
+	 */
+	HuskyHighlevelController(ros::NodeHandle& nodeHandle);
+
+	/*!
+	 * Destructor.
+	 */
+	virtual ~HuskyHighlevelController();
+
+private:
+	ros::NodeHandle nodeHandle_;
+	ros::Subscriber subscriber_;
+	ros::Publisher publisher_;
+	ros::Publisher vis_pub_;
+
+	// parameters
+  float p_gain;
+  float x_dot;
+
+	void topicCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+};
+
+} /* namespace */
